9b03a5d3d96ce610b14b0b79f5a800ed825a43c5
Elena <foxichenochek@yandex.ru>
2023-11-27 13:23:52 -0700

    Move and Jump realisation
    For CharacterController

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,17 +3,21 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3f;
-    [SerializeField] private float _straveSpeed = 3f;
+    [SerializeField] private float _speed = 8f;
+    [SerializeField] private float _strafeSpeed = 7f;
+    [SerializeField] private float _jumpSpeed = 7f;
+    [SerializeField] private float _gravityFactor = 2f;
+
     [SerializeField] private Transform _cameraTransform;
-    [SerializeField] private float _verticalTurnSensivity = 10f;
-    [SerializeField] private float _horizontalTurnSensitivity =10f;
+    [SerializeField] private float _horizontalTurnSensitivity = 10f;
+    [SerializeField] private float _verticalTurnSensitivity = 10f;
     [SerializeField] private float _verticalMinAngle = -89f;
     [SerializeField] private float _verticalMaxAngle = 89f;
 
     private Transform _transform;
     private CharacterController _characterController;
     private float _cameraAngle = 0f;
+    private Vector3 _verticalVelocity;
 
     private void Awake()
     {
@@ -24,43 +28,63 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        Vector3 forward = Vector3.ProjectOnPlane(_cameraTransform.forward, Vector3.up).normalized;
-        Vector3 right = Vector3.ProjectOnPlane(_cameraTransform.right, Vector3.up).normalized;
-
-        _cameraAngle -= Input.GetAxis("Mouse Y") * _verticalTurnSensivity;
-        _cameraAngle = Mathf.Clamp(_cameraAngle, _verticalMinAngle, _verticalMaxAngle);
-        _cameraTransform.localEulerAngles = Vector3.right * _cameraAngle;
-
-        if (_characterController != null)
+        if(_characterController != null)
         {
-            Vector3 playerSpeed = forward * Input.GetAxis("Vertical") * _speed + right * Input.GetAxis("Horizontal") * _straveSpeed;
-            playerSpeed *= Time.deltaTime;
+            Vector3 playerSpeed = GetPlayerSpeed();
 
-            if (_characterController.isGrounded)
-            {
-                _characterController.Move(playerSpeed + Physics.gravity);
-                //можно так
-                //_characterController.Move(playerSpeed + Vector3.down);
-            }
-            else
-            {
-                _characterController.Move(_characterController.velocity + Physics.gravity * Time.deltaTime);
-            }
+            VerticalRotate();
+            
+            HorizontalRotate();
+
+            Move(playerSpeed);
         }
     }
 
-    //Obstacle with rigidbody fly up when player collides it
-    //private void OnControllerColliderHit(ControllerColliderHit hit)
-    //{
-    //    if (hit != null && hit.rigidbody != null)
-    //        hit.rigidbody.velocity = Vector3.up * 100f;
-    //}
-
-    private void OnDrawGizmos()
+    private Vector3 GetPlayerSpeed()
     {
-        Gizmos.color = Color.blue;
-        var character = GetComponent<CharacterController>();
+        Vector3 forward = Vector3.ProjectOnPlane(_cameraTransform.forward, Vector3.up).normalized;
+        Vector3 right = Vector3.ProjectOnPlane(_cameraTransform.right, Vector3.up).normalized;
+        Vector3 playerSpeed = forward * Input.GetAxis("Vertical") * _speed +
+                          right * Input.GetAxis("Horizontal") * _strafeSpeed;
 
-        Gizmos.DrawWireCube(transform.position, Vector3.right + Vector3.forward + Vector3.up * character.height);
+        return playerSpeed * Time.deltaTime;
+    }
+
+    private void VerticalRotate()
+    {
+        _cameraAngle -= Input.GetAxis("Mouse Y") * _verticalTurnSensitivity;
+        _cameraAngle = Mathf.Clamp(_cameraAngle, _verticalMinAngle, _verticalMaxAngle);
+        _cameraTransform.localEulerAngles = Vector3.right * _cameraAngle;
+    }
+    
+    private void HorizontalRotate()
+    {
+        _transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * _horizontalTurnSensitivity);
+    }
+
+    private void Move(Vector3 playerSpeed)
+    {
+        if (_characterController.isGrounded)
+            HandleGroundedPlayerMovement(playerSpeed);
+        else
+            HandleAirborneMovement();
+    }
+
+    private void HandleAirborneMovement()
+    {
+        Vector3 horizontalVelocity = _characterController.velocity;
+        horizontalVelocity.y = 0;
+        _verticalVelocity += Physics.gravity * Time.deltaTime * _gravityFactor;
+        _characterController.Move((horizontalVelocity + _verticalVelocity) * Time.deltaTime);
+    }
+
+    private void HandleGroundedPlayerMovement(Vector3 playerSpeed)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            _verticalVelocity = Vector3.up * _jumpSpeed;
+        else
+            _verticalVelocity = Vector3.down;
+
+        _characterController.Move(playerSpeed + _verticalVelocity * Time.deltaTime);
     }
 }
